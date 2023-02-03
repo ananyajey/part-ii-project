1a8db80f7c8936630b20d6ea62f8ddaddbc0363c
ananyajey <ananya.jeyappragash@gmail.com>
2023-02-03 11:25:02 +0000

    fft initial implementation

--- cnn.py
+++ cnn.py
@@ -32,6 +32,11 @@ def image_to_array(image_filepath):
 
 
 def load_data(data_folderpath):
+    """
+    Parameters
+    ----------
+    
+    """
     data = {}
     for item in os.listdir(data_folderpath):
         item_path = os.path.join(data_folderpath, item)
@@ -61,6 +66,104 @@ sample_dict[3] = 3
 sample_dict[4] = 4
 
 print(split_data(sample_dict))
+
+
+
+
+
+
+
+
+
+
+
+# FFT stuff
+
+
+
+
+'''import numpy as np
+import matplotlib.pyplot as plt
+
+def stft(x, fs, window, overlap):
+    hop_size = window - overlap
+    window = np.hanning(window)
+    X = np.array([np.fft.fft(window * x[i:i+window]) for i in range(0, len(x)-window, hop_size)])
+    return X
+
+fs = 44100 # sample rate
+T = 5.0 # length of signal in seconds
+t = np.linspace(0, T, int(T*fs), endpoint=False)
+x = 0.5 * np.sin(2*np.pi*440*t) + np.sin(2*np.pi*880*t)
+
+X = stft(x, fs, 2048, 1024)
+f, ax = plt.subplots(figsize=(4,4))
+ax.matshow(np.abs(X[:,:400]), origin='lower', aspect='auto', cmap='viridis')
+ax.set_xlabel('Frame')
+ax.set_ylabel('Frequency [Hz]')
+plt.show()
+
+
+
+
+import cmath
+import math
+
+def fft(x):
+    N = len(x)
+    if N <= 1:
+        return x
+    even = fft(x[0::2])
+    odd = fft(x[1::2])
+    return [even[k] + cmath.exp(-2j*cmath.pi*k/N)*odd[k] for k in range(N//2)] + \
+           [even[k] - cmath.exp(-2j*cmath.pi*k/N)*odd[k] for k in range(N//2)]
+
+T = 5.0 # length of signal in seconds
+fs = 44100 # sample rate
+t = [i/fs for i in range(int(T*fs))]
+x = [0.5 * math.sin(2*math.pi*440*t_i) + math.sin(2*math.pi*880*t_i) for t_i in t]
+
+X = fft(x)
+X_magnitude = [math.sqrt(c.real**2 + c.imag**2) for c in X]
+X_magnitude = X_magnitude[:len(X_magnitude)//2]'''
+
+
+
+
+import numpy as np
+def DFT_slow(x):
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+
+print(DFT_slow([1, 2, 3]))
+print(np.fft.fft([1,2,3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
