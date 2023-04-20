158dd0450d43c9c2268873c23b58bf603d7ec61c
ananya <ananya.jeyappragash@gmail.com>
2023-04-20 21:29:49 +0100

    large dataset

--- visualization_new.py
+++ visualization_new.py
@@ -5,7 +5,7 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from PIL import Image
 import librosa
-import cupy as cp
+#import cupy as cp
 import os
 import threading
 
@@ -123,48 +123,82 @@ def wav_to_cochleagram():
 
 def split(image, id, clip_len, shift_len, resize_dim = None, pps = pps):
     '''
-    input - image opject
-    crop into squares
-    resize
-    reshape and remove opacity channel
+    
     '''
+    labels = None
+    images = None
+    output_dict = {}
+
     width, height = image.size
     left = 0
     right = left + clip_len * pps
     top = 0
     bottom = height
-    output = {}
+    
     while (right <= width):
         img = (image.crop((left, top, right, bottom)))
 
         if(resize_dim != None):
             img = img.resize((resize_dim, resize_dim))
+
+        img_array = np.array(img)
+        arr_norm = img_array/255.0
+        output_arr = arr_norm.reshape((arr_norm.shape[0], arr_norm.shape[1], 1))
+        '''if images is not None:
+            images = np.append(images, [output_arr], axis = 0)
+        else:
+            images = np.array([output_arr])'''
         
-        img_arr = np.array(img)/255.0
+        output_label = "{0}_{1}_{2}".format(id, (left/pps), (right/pps))
+        '''if labels is not None:
+            labels = np.append(labels, [output_label], axis = 0)
+        else:
+            labels = np.array([output_label])'''
+        
+        output_dict[output_label] = output_arr
+
+
+        
+        #output[output_label] = output_arr
+
+        '''
+        back to image:
+        Image.fromarray(output_arr.reshape((output_arr.shape[0], output_arr.shape[1])) * 255.0)
+        '''
+        
+        
+        
+        '''img_arr = np.array(img)#/255.0
         #img_arr_reshape = np.moveaxis(img_arr, -1, 0)
         #output_img = img_arr_reshape[:3]
         #output_img = np.array(img)
-        output_label = "{0}_{1}_{2}".format(id, int(left/pps), int(right/pps))
+        output_label = "{0}_{1}_{2}".format(id, (left/pps), (right/pps)) 
         output[output_label] = (img_arr/255.0).reshape((img_arr.shape[0], img_arr.shape[1], 1))
-        #print(left, right)
+        #print(left, right)'''
+
         left += shift_len * pps
         right += shift_len * pps
 
-    return output
+    #return images, labels
+    return output_dict
+
+
         
 
 
 
 
-for item in os.listdir(r"C:\Users\anany\Cambridge\Part II Project\data\raw\rock"):
+#for item in os.listdir(r"C:\Users\anany\Cambridge\Part II Project\data\small\rock"):
 
-    p = os.path.join(r"C:\Users\anany\Cambridge\Part II Project\data\raw\rock", item)
+
+#    p = os.path.join(r"C:\Users\anany\Cambridge\Part II Project\data\small\rock", item)
+    
 
         
-    asdf = wav_to_spectrogram(p)
+'''    asdf = wav_to_spectrogram(p)
 
-    jkl = split(asdf[0], "aaa", 10, 10)
+    jkl = split(asdf[0], "aaa", 1, 10, resize_dim=129)
 
-    print(item, "done")
+    print(item, "done")'''
 
 
