9fac84807426e6006453f5ea00245f788c09f00d
ananya <ananya.jeyappragash@gmail.com>
2023-04-23 18:06:29 +0100

    clean up

--- visualization_new.py
+++ visualization_new.py
@@ -25,41 +25,15 @@ def wav_to_spectrogram(wav_filepath, pps = pps):
     out = []
     
     for i in range(2):
-        #print(os.path.basename(wav_filepath), i)
         f, t, s = signal.stft(x = audio_data[i], fs = sample_rate)
-
         s_log = np.log(np.abs(s) + 1)
-
         scale = 255.0 / np.max(s_log)
-
         s_norm = (s_log * scale).astype(np.uint8)
-
         im = Image.fromarray(s_norm, mode='L')
-
         width, height = im.size
-
         new_width = int(pps * frame_count/sample_rate)
-
         im_resized = im.resize((new_width, height))
 
-
-
-        #with lock:
-        '''fig = plt.figure(frameon=False)#, figsize=(frame_count/sample_rate, 5), dpi=150) # figsize??
-        #im = plt.pcolormesh(t, f, np.log(np.abs(s) + 1))
-        im = plt.imshow(np.log(np.abs(s) + 1))
-        plt.axis('off')
-        plt.close()
-
-        img_buf = io.BytesIO()
-        fig.savefig(img_buf, format='png')
-        im = Image.open(img_buf)
-        im = im.crop(im.getbbox()) # remove whitespace
-
-        width, height = im.size
-
-        final_im = im.resize((pps * int(frame_count / sample_rate), height))
-        '''
         out.append(im_resized)
     return out
 
@@ -83,14 +57,7 @@ def wav_to_chromagram(wav_filepath, pps = pps):
         S = np.abs(librosa.stft(y[i]))
         chroma = librosa.feature.chroma_stft(S=S, sr=sr)
 
-
-
         #img = librosa.display.specshow(chromagram, y_axis='chroma', x_axis='time', ax=ax[1])
-
-
-        '''
-        '''
-
 
         img_buf = io.BytesIO()
         plt.savefig(img_buf, format='png')
@@ -102,16 +69,10 @@ def wav_to_chromagram(wav_filepath, pps = pps):
 
         out.append(final_im)
 
-
-
     #S = np.abs(librosa.stft(y))
     #chroma = librosa.feature.chroma_stft(S=S, sr=sr)
 
     x = 0
-
-
-    
-
 
 
 def wav_to_mfcc():
@@ -144,61 +105,14 @@ def split(image, id, clip_len, shift_len, resize_dim = None, pps = pps):
         img_array = np.array(img)
         arr_norm = img_array/255.0
         output_arr = arr_norm.reshape((arr_norm.shape[0], arr_norm.shape[1], 1))
-        '''if images is not None:
-            images = np.append(images, [output_arr], axis = 0)
-        else:
-            images = np.array([output_arr])'''
         
         output_label = "{0}_{1}_{2}".format(id, (left/pps), (right/pps))
-        '''if labels is not None:
-            labels = np.append(labels, [output_label], axis = 0)
-        else:
-            labels = np.array([output_label])'''
         
         output_dict[output_label] = output_arr
-
-
-        
-        #output[output_label] = output_arr
-
-        '''
-        back to image:
-        Image.fromarray(output_arr.reshape((output_arr.shape[0], output_arr.shape[1])) * 255.0)
-        '''
-        
-        
-        
-        '''img_arr = np.array(img)#/255.0
-        #img_arr_reshape = np.moveaxis(img_arr, -1, 0)
-        #output_img = img_arr_reshape[:3]
-        #output_img = np.array(img)
-        output_label = "{0}_{1}_{2}".format(id, (left/pps), (right/pps)) 
-        output[output_label] = (img_arr/255.0).reshape((img_arr.shape[0], img_arr.shape[1], 1))
-        #print(left, right)'''
 
         left += shift_len * pps
         right += shift_len * pps
 
     #return images, labels
     return output_dict
-
-
-        
-
-
-
-
-#for item in os.listdir(r"C:\Users\anany\Cambridge\Part II Project\data\small\rock"):
-
-
-#    p = os.path.join(r"C:\Users\anany\Cambridge\Part II Project\data\small\rock", item)
-    
-
-        
-'''    asdf = wav_to_spectrogram(p)
-
-    jkl = split(asdf[0], "aaa", 1, 10, resize_dim=129)
-
-    print(item, "done")'''
-
 
